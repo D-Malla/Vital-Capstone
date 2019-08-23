c0db67d73df3def293e36de064eac3c2d4eb476e
Dylan <dmalla@live.com>
2019-08-22 17:10:18 -0700

    Background image changed, overlay added

--- client/src/components/Home/HomeBody.js
+++ client/src/components/Home/HomeBody.js
@@ -4,6 +4,7 @@ export default props => {
   return (
     <div id='homeBodyContainer'>
       <div className='homeImage'></div>
+      <div className='imgOverlay'></div>
       <div className='classSelectorDiv'>
         <div className='classSelector'><h3>HTML</h3></div>
         <div className='classSelector'><h3>CSS</h3></div>
