392e7cfbcbadb6787c024685e81462f9b3466e5f
Geoff Hutson <geoff.hutson.89@gmail.com>
2019-08-27 23:36:56 -0700

    working on button rotation

--- client/src/components/LessonPage/LessonBody.js
+++ client/src/components/LessonPage/LessonBody.js
@@ -6,11 +6,8 @@ import ReactMarkdown from "react-markdown";
 import { getLessonTitles, getLessonData } from "../../actions/vital.actions";
 
 export default props => {
-  // const title = useSelector(appState => appState.lessons_title);
   const lessons_list = useSelector(appState => appState.get_lessons);
   const lesson_data = useSelector(appState => appState.lesson_data);
-  // const lesson = props.props.match.params.lesson;
-  // const parent_id_new = props.props.match.params.parent_id;
   const id = props.props.match.params.id;
   const prev_id = Number(props.props.match.params.inid) - 1;
   const next_id = Number(props.props.match.params.inid) + 1;
@@ -49,8 +46,14 @@ export default props => {
       <div>
         <button type="button">
           <Link to={"/lesson/" + id + "/" + prev_id}>PREV</Link>
+
+          {/* <Link to={"/lesson/" + prev_cat + "/" + prev_id} /> */}
         </button>
-        <button type="button">
+        <button
+          className="next-category"
+          type="button"
+          disabled={next_id == 40}
+        >
           <Link to={"/lesson/" + id + "/" + next_id}>CONTINUE</Link>
         </button>
       </div>
